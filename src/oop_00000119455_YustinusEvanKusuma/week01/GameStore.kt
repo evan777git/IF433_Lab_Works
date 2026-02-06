72c9be33a578d4197c2940f6df2b49dc15a17165
evan777git <evankusuma777@gmail.com>
2026-02-06 16:16:17 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000119455_YustinusEvanKusuma/week01/GameStore.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week01/GameStore.kt
@@ -3,6 +3,18 @@ package oop_00000119455_YustinusEvanKusuma.week01
 fun main(){
     val gameTitle = "GTA"
     val price = 250_000
+
+    val finalPrice = calculateDiscount(price)
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
-fun calculateDiscount(price: Int) = if (price > 500_000) price * 0.2 else price * 0.1 
+fun calculateDiscount(price: Int) = if (price > 500_000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(
+    title: String,
+    finalPrice: Int
+) {
+    println("Judul Game  : $title")
+    println("Harga Akhir: Rp $finalPrice")
+} 
