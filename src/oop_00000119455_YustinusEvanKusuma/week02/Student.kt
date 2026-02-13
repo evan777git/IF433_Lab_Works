1e1e2137b207b3eea3a4f76b7d94a6f2ece765b2
evan777git <evankusuma777@gmail.com>
2026-02-13 15:47:45 +0700

    week02: add secondary constructor

--- src/oop_00000119455_YustinusEvanKusuma/week02/Student.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week02/Student.kt
@@ -14,4 +14,9 @@ class Student (
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
+    //Secondary Constructor
+    //Wajib memanggil Primary Constructor menggunakan 'this()'
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
