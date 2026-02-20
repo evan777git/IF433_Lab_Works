e788458121efb9bc45425f86794d359a8fbbd768
evan777git <evankusuma777@gmail.com>
2026-02-20 15:19:30 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000119455_YustinusEvanKusuma/week03/Employee.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week03/Employee.kt
@@ -2,4 +2,10 @@ package oop_00000119455_YustinusEvanKusuma.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // PERINGATAN: KODE DIBAWAH INI SALAH DAN BERBAHAYA!
+            // Kita memanggil setter di dalam setter (Recursive)
+            this.salary = value
+        }
 }
