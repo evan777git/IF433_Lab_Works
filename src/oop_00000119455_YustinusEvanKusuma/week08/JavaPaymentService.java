c56aa3c0fa08d06426a0a2e610ec83d7d33de556
evan777git <evankusuma777@gmail.com>
2026-04-17 15:45:01 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000119455_YustinusEvanKusuma/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000119455_YustinusEvanKusuma.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
