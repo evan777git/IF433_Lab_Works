f495611a1eafd93609540ad4680940f79224e6cb
evan777git <evankusuma777@gmail.com>
2026-04-24 15:13:42 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000119455_YustinusEvanKusuma/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000119455_YustinusEvanKusuma.week09
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 50, -12.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, 25.4, "OPEN"),
+        TradeLog("DOGEUSDT", "SHORT", 5, 8.5, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 20, 4.2, "OPEN")
+    )
+}
