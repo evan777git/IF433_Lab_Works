3bf41802037d19d5cf8e630aafc850fbe8d36131
evan777git <evankusuma777@gmail.com>
2026-05-07 13:34:35 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000119455_YustinusEvanKusuma/week10/WalletModels.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week10/WalletModels.kt
@@ -1,4 +1,9 @@
 package oop_00000119455_YustinusEvanKusuma.week10
 
-data class Coin(val name: String, val balance: Double)
+// Untuk filter pencarian berdasarkan nama
+interface INamedEntity {
+    val name: String
+}
+
+data class Coin(override val name: String, val balance: Double): INamedEntity
 data class Transaction(val id: String, val amount: Double)
